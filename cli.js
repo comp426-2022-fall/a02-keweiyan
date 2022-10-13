d8badae4f6d8c00def20c9890cc624621109c13e
keweiyan <keweiyan@live.unc.edu>
2022-10-13 16:37:19 -0400

    changed timezone

--- cli.js
+++ cli.js
@@ -24,26 +24,24 @@ console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME
 let latitude = 35.92
 if (args.n) {
     latitude = args.n
-}
-if (args.s) {
-    latitude = args.s
+} else if (args.s) {
+    latitude = -1 * args.s
 }
 // Declare longitude
 let longitude = -79.05
-if (args.w) {
-    latitude = args.w
-}
 if (args.e) {
     latitude = args.e
+} else if (args.w) {
+    latitude = -1 * args.w
 }
 // Declare timezone
-const timezone = moment.tz.guest();
-if (args.t) {
-    timezone = args.t
+let timezone = args.z
+if (timezone == null) {
+  timezone = moment.tz.guess();
 }
 
 // Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7')
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_sum&timezone=' + timezone)
 // Get the data from the request
 const data = await response.json();
 // Log the data onto STDOUT
